7755f897ec765ce1cb57766d1c10709d76042ba3
rachitkumar14 <rachitk288@gmail.com>
2023-08-06 23:37:26 +0530

    six projects first commit

---
+++ Fourth Project/fourth.js
@@ -0,0 +1,7 @@
+let dropbtn=document.querySelector("#dropdown");
+dropbtn.addEventListener("click",function()
+{
+ let entry= document.querySelector(".toggles");
+ entry.classList.toggle('active');
+
+})
---
+++ Sixth Project/sixth.js
@@ -0,0 +1,12 @@
+// let like_button=document.querySelector('#heart');
+// like_button.addEventListener('click',function(){
+//     like_button.classList.toggle('fa-solid');
+//     if(like_button.classList=="fa-regular fa-heart fa-solid")
+//     {
+//         like_button.style.color="red";
+//     }
+//     else{
+//         like_button.style.color="white";
+//     }
+//     console.log("clicked");
+// })
